4eb0a0934c220d1e0c2daac4e3d3c1fc54b7d6bc
Marco A. Amez <marco.a.amez@gmail.com>
2017-03-13 11:08:26 +0100

    Add authentication

---
+++ localConfig.js
@@ -0,0 +1,9 @@
+"use strict";
+
+module.exports = {
+
+    jwt: {
+        secret: 'ñlfakdsfñweli8urp9w83ijrwo8rypweoihrqwaep98r',
+        expiresIn: '2 days'
+    }
+};
--- routes/api/usuarios.js
+++ routes/api/usuarios.js
@@ -6,6 +6,14 @@
 const express = require('express');
 const router = express.Router();
 
+const hash = require('hash.js');
+
+const jwt = require('jsonwebtoken');
+
+const localConfig = require('../../localConfig');
+
+const jwtAuth = require('../../lib/jwtAuth');
+
 const mongoose = require('mongoose');
 const Usuario = mongoose.model('Usuario'); // como mongoose ya tiene definido el modelo, se lo pedimos
 
@@ -22,5 +30,39 @@ router.post('/register', function (req, res, next) {
 
 
 })
+
+router.post('/authenticate', function (req, res, next){
+    const clave = req.body.clave;
+    const email = req.body.email;
+
+    Usuario.findOne({email: email}, function (err, user) {
+
+        if (err) {
+            next(err);
+            return;
+        }
+
+        //codificamos la contraseña con el hash
+        const claveCodificada = hash.sha256().update(clave).digest('hex');
+
+        //comprobamos que coincida
+        if (claveCodificada == user.clave) {
+            // hacemos un token
+            const token = jwt.sign({email: user.email}, localConfig.jwt.secret, {
+                expiresIn: localConfig.jwt.expiresIn
+            });
+
+            res.json({success: true, token: token});
+
+        } else {
+            return res.json({
+                ok: false, error: {
+                    code: 401,
+                    message: 'password incorrecto'
+                }
+            });
+        }
+    });
+});
 
 module.exports = router;
