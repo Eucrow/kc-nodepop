141466e8c74f07bbedf3315d9cbacbe46822b6de
Marco A. Amez <marco.a.amez@gmail.com>
2017-03-14 21:11:12 +0100

    Minor changes

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
-var i18n = require("i18n");
+var i18n = require('i18n');
 
 // base de datos y modelos
-require('./lib/connectMongoose');  //se ejecuta el código que hay en connectMongoose.js, que lo que hace es arrancar la
+require('./lib/connectMongoose');  // se ejecuta el código que hay en connectMongoose.js, que lo que hace es arrancar la
                                    // conexión con la base de datos
 require('./models/Anuncio'); // lo cargamos para que mongoose sepa que existe el modelo
 require('./models/Usuario');
@@ -23,14 +22,14 @@ app.set('view engine', 'ejs');
 
 // configure locales with i18n
 i18n.configure({
-    locales:['en', 'es'],
-    directory: __dirname + '/locales',
-    register: global
+  locales: ['en', 'es'],
+  directory: path.join(__dirname, '/locales'),
+  register: global
 });
 app.use(i18n.init);
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -44,14 +43,14 @@ app.use('/apiv1/usuarios', require('./routes/apiv1/usuarios'));
 app.use('/apiv1/anuncios/tags', require('./routes/apiv1/anuncios'));
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
